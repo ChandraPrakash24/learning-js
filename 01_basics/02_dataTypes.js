d9b9eb919e7c484d15d1a22584133232993886ba
ChandraPrakash24 <2412prakas@gmail.com>
2023-10-14 19:24:55 +0530

    02_dataType

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,18 @@
+"use strict"; // treat all JS code as newar version (use of arrow fun, objects, etc) by default it is on every time not needed to write this it is just a good practice
+
+// alert(2 + 2); // wont't work here coz we are using node js not browser
+
+//    PREMIIVE DATA TYPE
+
+
+// number (typeof--> number) ==> let num = 2344;
+// bigInt (typeof--> number) ==> let longNo = 765675544332434534543;
+// string (typeof--> string) ==> let city = "Indore";
+// boolean (typeof--> number) ==> let isLogged = true;
+// undefined (typeof--> undefined) ==> let accountCity; // only declare np value is asigned
+// null (typeof--> object) ==> let temp = null; // ex.., like some time we dont want like server to return temp `0` in case of server error coz `0` itself is a temperature so we want at that case to return `null`
+
+
+//      NO PREMATIVE DTATA TYPE
+
+// objects
