d15a374679a1fe0dea5de17a99d46a7876b8edb6
ChandraPrakash24 <2412prakas@gmail.com>
2023-10-20 18:02:42 +0530

    conversions of data types

--- 01_basics/02_dataTypes.js
+++ 01_basics/02_dataTypes.js
@@ -11,6 +11,7 @@
 // boolean (typeof--> number) ==> let isLogged = true;
 // undefined (typeof--> undefined) ==> let accountCity; // only declare np value is asigned
 // null (typeof--> object) ==> let temp = null; // ex.., like some time we dont want like server to return temp `0` in case of server error coz `0` itself is a temperature so we want at that case to return `null`
+// symbol ==> unique
 
 
 // NAN ex; "123abc"
--- 01_basics/03_conversionsAndOperations.js
+++ 01_basics/03_conversionsAndOperations.js
@@ -1,7 +1,24 @@
-let num = 54;
+let stringNum = "33abc";
 
-console.log(typeof num);
+let stringInNum = Number (stringNum);
+console.log(typeof stringInNum); //type: number
+console.log(stringInNum); //value: NaN
 
-let numtostring = String(num);
+// "123" => 123
+// "123ABC" => NaN
+// "true" => 1 / "false" => 0
 
-console.log(typeof numtostring);
+let isLoggedIn = 1; // => true
+// let isLoggedIn = 0; // => false
+// let isLoggedIn = ""; // => false
+// let isLoggedIn = "abcd"; // => true
+
+let boolianIsLOggedIn = Boolean(isLoggedIn);
+console.log(typeof boolianIsLOggedIn); // type: boolean
+console.log(boolianIsLOggedIn); // value: true
+
+let someNumber = 123;
+
+let stringNumber = String(someNumber);
+console.log(typeof stringNumber); // type: string
+console.log(stringNumber); // value: 123
