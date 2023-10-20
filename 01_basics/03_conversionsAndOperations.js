2faa406acf1ce011c3307b00f899a0fd365f57ef
ChandraPrakash24 <2412prakas@gmail.com>
2023-10-20 18:31:27 +0530

    operations

--- 01_basics/03_conversionsAndOperations.js
+++ 01_basics/03_conversionsAndOperations.js
@@ -1,6 +1,8 @@
+// ----------------- CONVERSIONS --------------------
+
 let stringNum = "33abc";
 
-let stringInNum = Number (stringNum);
+let stringInNum = Number(stringNum);
 console.log(typeof stringInNum); //type: number
 console.log(stringInNum); //value: NaN
 
@@ -22,3 +24,46 @@ let someNumber = 123;
 let stringNumber = String(someNumber);
 console.log(typeof stringNumber); // type: string
 console.log(stringNumber); // value: 123
+
+
+// ----------- OPERATIONS --------------------
+
+
+let positiveNum = 123;
+let negativeNum = -positiveNum;
+console.log(negativeNum); // -123
+
+console.log(2 + 2); // 4
+console.log(2 - 2); // 0
+console.log(2 * 2); // 4
+console.log(2 ** 3); // 4 (2^3)
+console.log(2 / 2); // 1 (quotient)
+console.log(2 % 2); // 0 (reminder)
+
+let str1 = "Jai Shree";
+let str2 = " Ram";
+let str3 = str1 + str2;
+console.log(str3); // Jai Shree Ram
+
+//    complex situation (also bad practice)
+
+// ToPrimitive in ecmascript (doc)
+
+console.log("1" + 2); // 12
+console.log(1 + "2"); // 12
+console.log("1" + 2 + 3); // 123
+console.log(2 + 2 + "3"); // 43
+
+// use perenthisis
+console.log(3 + 4 * 5 % 3); // v v bad practice // 5 [wrong]
+console.log(((3 + 4) * 5) % 3); // good // 2 [right]
+
+// bad code: (never do this)
+// confusing code
+
+console.log(true); // true
+console.log(+true); // 1
+console.log(+""); // 0
+
+let num1, num2, num3;
+num1 = num2 = num3 = 2 + 2; //(why to do this man?)
